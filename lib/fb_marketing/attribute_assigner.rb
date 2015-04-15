8963647cf56d967b91a9e5156176d22a7209b282
Carl Partridge <CPartridge@sparknettech.com>
2015-04-15 13:07:07 -0400

    no message

--- lib/fb_marketing/attribute_assigner.rb
+++ lib/fb_marketing/attribute_assigner.rb
@@ -27,7 +27,7 @@ module FbMarketing
             when :int_flag
               raw == 1
             when :json 
-              raw.as_json
+              raw.to_json
             when :iso8601
               if raw == 0 # catch end_time of 0 (FB default for no end_time)
                 0
--- lib/fb_marketing/struct.rb
+++ lib/fb_marketing/struct.rb
@@ -1,6 +1,12 @@
 module FbMarketing
 	class Struct < FbGraph2::Struct
+		def self.inherited(klass)
+			klass.send :include, AttributeAssigner
+		end
 
+		def initialize(attributes = {})
+			assign attributes if respond_to? :assign
+		end
 	end
 end
 
