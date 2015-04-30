c0189bef310f6cf85b37382c61f0c98be1cc3091
Carl Partridge <CPartridge@sparknettech.com>
2015-04-30 10:01:47 -0400

    no message

--- lib/fb_marketing/ad_campaign.rb
+++ lib/fb_marketing/ad_campaign.rb
@@ -4,12 +4,12 @@ module FbMarketing
 		register_attributes(
 			raw: [
 				:campaign_group_status,
-				:name#,
+				:name,
 				# :buying_type,
 				# :execution_options, # array
-				# :objective
+				:objective
 			]
-	    )
+	   )
 
 		def create(options = {})
 	   	self.id = "act_" + self.id + "/adcampaign_groups"
