ff33cbf819aa4f207e4b3ff3bc80b5dce30dcb7f
Carl Partridge <CPartridge@sparknettech.com>
2015-05-05 15:07:49 -0400

    no message

--- lib/fb_marketing/targeting_search.rb
+++ lib/fb_marketing/targeting_search.rb
@@ -16,44 +16,45 @@ module FbMarketing
 				:audience_size, # type=adTargetingCategory
 				:description, # type=adTargetingCategory
 				:class#,  # type=adTargetingCategory
-			],
-			simple_array: [
-				:countries,
-				:regions,
-				:cities,
-				:zips,
-				:location_types, # type=adgeolocation
-				:interest_list, #type=adinterestsuggestion
-				:path#, # type=adTargetingCategory
 			]
+			# ],
+			# simple_array: [
+			# 	:countries,
+			# 	:regions,
+			# 	:cities,
+			# 	:zips,
+			# 	:location_types, # type=adgeolocation
+			# 	:interest_list, #type=adinterestsuggestion
+			# 	:path#, # type=adTargetingCategory
+			# ]
 		)
 
-		def initialize(id, attributes = {})
-	    	super
-	    	Rails.logger.error "TARGETING SEARCH: #{self.inspect}"
-	    	self.id = "search"
-	    	# if attributes.include?(:countries)
-	    	# 	self.countries = FbGraph2::Collection.new(attributes[:countries])
-	    	# end
-	    	# if attributes.include?(:regions)
-	    	# 	self.regions = FbGraph2::Collection.new(attributes[:regions])
-	    	# end
-	    	# if attributes.include?(:cities)
-	    	# 	self.cities = FbGraph2::Collection.new(attributes[:cities])
-	    	# end
-	    	# if attributes.include?(:zips)
-	    	# 	self.zips = FbGraph2::Collection.new(attributes[:zips])
-	    	# end
-	    	# if attributes.include?(:location_types)
-	    	# 	self.location_types = FbGraph2::Collection.new(attributes[:location_types])
-	    	# end
-	    	# if attributes.include?(:interest_list)
-	    	# 	self.interest_list = FbGraph2::Collection.new(attributes[:interest_list])
-	    	# end
-	    	# if attributes.include?(:path)
-	    	# 	self.path = FbGraph2::Collection.new(attributes[:path])
-	    	# end
-	   end
+		# def initialize(id, attributes = {})
+	 #    	super
+	 #    	Rails.logger.error "TARGETING SEARCH: #{self.inspect}"
+	 #    	self.id = "search"
+	 #    	# if attributes.include?(:countries)
+	 #    	# 	self.countries = FbGraph2::Collection.new(attributes[:countries])
+	 #    	# end
+	 #    	# if attributes.include?(:regions)
+	 #    	# 	self.regions = FbGraph2::Collection.new(attributes[:regions])
+	 #    	# end
+	 #    	# if attributes.include?(:cities)
+	 #    	# 	self.cities = FbGraph2::Collection.new(attributes[:cities])
+	 #    	# end
+	 #    	# if attributes.include?(:zips)
+	 #    	# 	self.zips = FbGraph2::Collection.new(attributes[:zips])
+	 #    	# end
+	 #    	# if attributes.include?(:location_types)
+	 #    	# 	self.location_types = FbGraph2::Collection.new(attributes[:location_types])
+	 #    	# end
+	 #    	# if attributes.include?(:interest_list)
+	 #    	# 	self.interest_list = FbGraph2::Collection.new(attributes[:interest_list])
+	 #    	# end
+	 #    	# if attributes.include?(:path)
+	 #    	# 	self.path = FbGraph2::Collection.new(attributes[:path])
+	 #    	# end
+	 #   end
 
 	end
 end
